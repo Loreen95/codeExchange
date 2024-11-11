7da5fae758280d07f22d2e25bec9d38f6f2eb389
David Kuiper <david.kuiper@hva.nl>
2024-11-11 15:51:52 +0100

    func add code that checks if any input is empty and sees if password is safe enough

--- src/controllers/RegisterController.ts
+++ src/controllers/RegisterController.ts
@@ -1,8 +1,4 @@
 class RegistrationClass {
-    public cough(): void {
-        console.log("*Cough cough");
-    }
-
     // public echo(lineup: string[]): void {
     //     const severoaubarka: HTMLParagraphElement = document.querySelector("#severo")!;
     //     for (let j: number = 0; j < lineup.length; j++) {
@@ -12,17 +8,49 @@ class RegistrationClass {
     //         console.log(lineup[j]);
     //     }
     // }
-    public emailVerify(userInputEmail: string): void {
-        const errorMessage: HTMLParagraphElement = document.querySelector("#errMsg")!;
-        console.log(`Email Verification Initialised...${userInputEmail}`);
-        if (!userInputEmail) {
-            errorMessage.innerText = "You'll have to give me an email if you want to join the comunnity";
+    private reasonOfWeakness: string = "";
+    public passChecker(givenPassword: string): boolean {
+        console.log("trigger1");
+        if (givenPassword.length < 7) {
+            this.reasonOfWeakness = "your password is too short. Use at least 7 caracers";
+            return false;
         }
-        else if (!userInputEmail.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
-            errorMessage.innerText = "The email provided here is unvalid";
+        else if (givenPassword.length > 20) {
+            this.reasonOfWeakness = "Your password is too long. Use 20 caracters at max";
+            return false;
         }
         else {
-            errorMessage.innerHTML = "he";
+            const streangth: boolean = Boolean(givenPassword.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,}$/));
+
+            if (streangth) {
+                return true;
+            }
+            else {
+                this.reasonOfWeakness = "Your password isn't strong enough. Use lowercase, uppercase, numbers and special caracters";
+                return false;
+            }
+        }
+    }
+
+    public verifyCridentials(userInputName: string, userInputEmail: string, userInputPassword: string): void {
+        const errorMessage: HTMLParagraphElement = document.querySelector("#errMsg")!;
+        if (!userInputName) {
+            errorMessage.innerText = "You must provide a name";
+        }
+        else if (!userInputEmail) {
+            errorMessage.innerText = "You'll have to give me an email if you want in";
+        }
+        else if (!userInputPassword) {
+            errorMessage.innerText = "Be sure to provide a password";
+        }
+        else if (!this.passChecker(userInputPassword)) {
+            errorMessage.innerText = String(this.reasonOfWeakness);
+        }
+        else if (!userInputEmail.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            errorMessage.innerText = "The provided email is invalid";
+        }
+        else {
+            errorMessage.innerHTML = "momentairy sucsess";
         }
     }
 }
