38b31d9a271b2e51fa59c6b931ac46617d8106ff
David Kuiper <david.kuiper@hva.nl>
2024-11-14 09:32:35 +0100

    fix: add two comments to logout vieuw

--- src/controllers/LogoutController.ts
+++ src/controllers/LogoutController.ts
@@ -1,5 +1,7 @@
+// No clue why this exists. This could easily be argued as part of the login controller, but the diagram is king
 export class LogoutClass {
     public logoutFunction(): void {
+        // Well this here, uh.. yeah, this just loggs you out. Idunno what to say.
         try {
             if (sessionStorage.getItem("session")) {
                 if (confirm("Proceed with logout?")) {
@@ -7,6 +9,7 @@ export class LogoutClass {
                     window.location.href = "http://localhost:3000/landingspagina.html";
                 }
             }
+            // This shouldn't be possible because the session needs to exist before you have access to the logout button
             else {
                 console.error("Fout met uitloggen");
             }
