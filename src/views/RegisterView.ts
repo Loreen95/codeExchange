2d6e1ac0db5ed5aace657bef07f1264249f43b89
David Kuiper <david.kuiper@hva.nl>
2024-11-14 09:26:51 +0100

    fix: add comments to register controller and vieuw

--- src/views/RegisterView.ts
+++ src/views/RegisterView.ts
@@ -1,10 +1,13 @@
+// this links the vieuw to the controller
 const Registration: RegistrationClass = new RegistrationClass();
 import RegistrationClass from "../controllers/RegisterController";
 
+// This collects all input data to be shipped off to the controller
 const emailAdressUserInput: HTMLInputElement = document.querySelector("#emailInput")!;
 const nameUserInput: HTMLInputElement = document.querySelector("#userName")!;
 const passwordUserInput: HTMLInputElement = document.querySelector("#password")!;
 
+// And this initiates the registration process,
 const registerBttn: HTMLButtonElement = document.querySelector(".registerNewUser")!;
 registerBttn.addEventListener("click", async () => {
     await Registration.onClickRegister(nameUserInput.value, emailAdressUserInput.value, passwordUserInput.value);
