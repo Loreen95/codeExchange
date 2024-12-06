73b6545cd5a862046dbe453c8425092119fba176
David Kuiper <david.kuiper@hva.nl>
2024-12-06 13:09:55 +0100

    fix: updated evidence

--- docs/Evidence locker/Anguish.ts
+++ docs/Evidence locker/Anguish.ts
@@ -6,91 +6,113 @@ enum MATEREAL {
 }
 
 abstract class Pakagin {
-    private matereal: MATEREAL;
+    private _matereal: MATEREAL;
 
     protected constructor(matereal1: MATEREAL) {
-        this.matereal = matereal1;
+        this._matereal = matereal1;
     }
 
     protected getMatereal(): MATEREAL {
-        return this.matereal;
+        return this._matereal;
     }
 
     protected abstract toString(): string;
 }
 
 class Flask extends Pakagin {
-    private  static amontOfFlasks: number = 1;
-    private maxVolumeInML: number;
-    private volumeInML: number = 0;
+    private static _amontOfFlasks: number = 1;
+    private _maxVolumeInML: number;
+    private _volumeInML: number = 0;
 
     public constructor(maxVolumeInML1: number) {
         super(MATEREAL.PLASTIC);
-        this.maxVolumeInML = maxVolumeInML1;
+        this._maxVolumeInML = maxVolumeInML1;
     }
 
     public getAmountOfContainers(): number {
-        return Flask.amontOfFlasks;
+        return Flask._amontOfFlasks;
     }
 
     public fill(milliLiters: number): void {
-        if (milliLiters > this.maxVolumeInML) {
-            console.log(`${milliLiters - this.maxVolumeInML} Spilled out`);
-            this.maxVolumeInML = milliLiters;
+        if (milliLiters > this._maxVolumeInML) {
+            console.log(`${milliLiters - this._maxVolumeInML} Spilled out`);
+            this._maxVolumeInML = milliLiters;
             return;
         }
-        this.volumeInML += milliLiters;
+        this._volumeInML += milliLiters;
     }
 
     public remove(milliLiters: number): void {
-        if (this.volumeInML - milliLiters < 0) {
-            this.volumeInML = 0;
-            console.log(`you only receive ${milliLiters - this.volumeInML} from the flask before it dries up`);
+        if (this._volumeInML - milliLiters < 0) {
+            this._volumeInML = 0;
+            console.log(`you only receive ${milliLiters - this._volumeInML} from the flask before it dries up`);
             return;
         }
-        this.volumeInML -= milliLiters;
+        this._volumeInML -= milliLiters;
     }
 
     public toString(): string {
         return `
             Flask info:
-            amount of flasks: ${Flask.amontOfFlasks}
-            volume of each individual flask: ${this.maxVolumeInML}
-            The contents of this flask: ${this.volumeInML}
+            amount of flasks: ${Flask._amontOfFlasks}
+            volume of each individual flask: ${this._maxVolumeInML}
+            The contents of this flask: ${this._volumeInML}
         `;
     }
 }
 
 class MilkFlask extends Pakagin {
-    private maxVolumeInLiters: number;
-    private volumeInLiters: number = 0;
+    private _maxVolumeInLiters: number;
+    private _volumeInLiters: number = 0;
 
     public constructor(maxVolumeInMiliLiters: number) {
         super(MATEREAL.DUST);
-        this.maxVolumeInLiters = (maxVolumeInMiliLiters / 1000);
+        this._maxVolumeInLiters = (maxVolumeInMiliLiters / 1000);
     }
 
     public toString(): string {
         return `
             Milkflask class:
-            volume of each individual cow flask: ${this.maxVolumeInLiters}
-            The contents of this cow flask: ${this.volumeInLiters}
+            volume of each individual cow flask: ${this._maxVolumeInLiters}
+            The contents of this cow flask: ${this._volumeInLiters}
         `;
     }
 }
 
+class CrateFlask extends Flask {
+    public constructor(maxVolumeInCL: number) {
+        super(maxVolumeInCL / 10);
+    }
+}
+
 class Crate extends Pakagin {
-    private amountOfFlasks: number;
+    private _maxAmountOfFlasks: number;
+    private _crateContents: CrateFlask[] = [];
 
     public constructor(amountOfFlasks1: number) {
         super(MATEREAL.CARDBOARD);
-        this.amountOfFlasks = amountOfFlasks1;
+        this._maxAmountOfFlasks = amountOfFlasks1;
+    }
+
+    /**
+     * name
+     */
+    public addItemToCrte(itemToBeAdded: CrateFlask): boolean {
+        if (this._crateContents.length < this._maxAmountOfFlasks) {
+            this._crateContents.push(itemToBeAdded);
+            console.log(`${itemToBeAdded} has been added to crate`);
+            return true;
+        }
+        else {
+            console.log(`${itemToBeAdded} has NOT been added to crate cus it's full!`);
+            return false;
+        }
     }
 
     public toString(): string {
         return `
             Crate class;
-            Amount of flasks in crate: ${this.amountOfFlasks}
+            Amount of flasks that could be in crate: ${this._maxAmountOfFlasks}
         `;
     }
 }
